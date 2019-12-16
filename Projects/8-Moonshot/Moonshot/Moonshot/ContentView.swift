3941347eeaa9373f7a72b03df1219c67fad31511
nestorivanmo <nestor.martinez.98@hotmail.com>
2019-12-15 22:13:08 -0600

    reset to clean state

--- Projects/8-Moonshot/Moonshot/Moonshot/ContentView.swift
+++ Projects/8-Moonshot/Moonshot/Moonshot/ContentView.swift
@@ -8,35 +8,9 @@
 
 import SwiftUI
 
-struct User: Codable {
-    var name: String
-    var address: Address
-}
-
-struct Address: Codable {
-    var street: String
-    var city: String
-}
-
 struct ContentView: View {
     var body: some View {
-        Button("Decode JSON") {
-            let input = """
-            {
-                "name": "Taylor Swift",
-                "address": {
-                    "street": "555, Taylor Swift Avenue",
-                    "city": "Nashville"
-                }
-            }
-            """
-            
-            let data = Data(input.utf8)
-            let decoder = JSONDecoder()
-            if let user = try? decoder.decode(User.self, from: data) {
-                print(user.address.street)
-            }
-        }
+        Text("Hello SwiftUI")
     }
 }
 
