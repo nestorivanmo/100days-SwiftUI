e4d535fe82365cfedd189f7d791c5fd3dacd07a7
nestorivanmo <nestor.martinez.98@hotmail.com>
2020-01-02 00:17:15 -0600

    checking for valid address

--- Projects/10-Cupcake/CupcakeCorner/CupcakeCorner/CheckoutView.swift
+++ Projects/10-Cupcake/CupcakeCorner/CupcakeCorner/CheckoutView.swift
@@ -9,6 +9,8 @@
 import SwiftUI
 
 struct CheckoutView: View {
+    @ObservedObject var order: Order
+    
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
@@ -16,6 +18,6 @@ struct CheckoutView: View {
 
 struct CheckoutView_Previews: PreviewProvider {
     static var previews: some View {
-        CheckoutView()
+        CheckoutView(order: Order())
     }
 }
