99b48eac237ad65bc5a3dc9583b8281dd358e61c
nestorivanmo <nestor.martinez.98@hotmail.com>
2020-01-08 10:26:06 -0600

    added NSPredicates

--- Projects/12-CoreData-TechniqueProject/CoreDataProject/CoreDataProject/ContentView.swift
+++ Projects/12-CoreData-TechniqueProject/CoreDataProject/CoreDataProject/ContentView.swift
@@ -5,31 +5,44 @@
 //  Created by Néstor I. Martínez Ostoa on 07/01/20.
 //  Copyright © 2020 Néstor I. Martínez Ostoa. All rights reserved.
 //
-
+import CoreData
 import SwiftUI
 
 struct ContentView: View {
     @Environment(\.managedObjectContext) var moc
-    @FetchRequest(entity: Wizard.entity(), sortDescriptors: []) var wizards: FetchedResults<Wizard>
+    @FetchRequest(entity: Ship.entity(), sortDescriptors: [], predicate: NSPredicate(format: "NOT name BEGINSWITH %@", "E")) var ships: FetchedResults<Ship>
     
+//    NSPredicate(format: "universe == %@", "Star Wars"))
+//    NSPredicate(format: "name < %@", "F"))
+//    NSPredicate(format: "universe IN %@", ["Aliens", "Firefly", "Star Trek"])
+//    NSPredicate(format: "name BEGINSWITH %@", "E"))
+//    NSPredicate(format: "name BEGINSWITH[c] %@", "e"))
+//    NSPredicate(format: "NOT name BEGINSWITH[c] %@", "e"))
+
     var body: some View {
         VStack {
-            List(wizards, id: \.self) { wizard in
-                Text(wizard.name ?? "Unknown")
+            List(ships, id: \.self) { ship in
+                Text(ship.name ?? "Unknown name")
             }
-            Button("Add") {
-                let wizard = Wizard(context: self.moc)
-                wizard.name = "Harry Potter"
+            Button("Add Examples") {
+                let ship1 = Ship(context: self.moc)
+                ship1.name = "Enterprise"
+                ship1.universe = "Star Trek"
+
+                let ship2 = Ship(context: self.moc)
+                ship2.name = "Defiant"
+                ship2.universe = "Star Trek"
+
+                let ship3 = Ship(context: self.moc)
+                ship3.name = "Millennium Falcon"
+                ship3.universe = "Star Wars"
+
+                let ship4 = Ship(context: self.moc)
+                ship4.name = "Executor"
+                ship4.universe = "Star Wars"
+
+                try? self.moc.save()
             }
-            
-            Button("Save") {
-                do {
-                    try self.moc.save()
-                } catch {
-                    print(error.localizedDescription)
-                }
-            }
-            
         }
     }
 }
--- Projects/12-CoreData-TechniqueProject/CoreDataProject/CoreDataProject/SceneDelegate.swift
+++ Projects/12-CoreData-TechniqueProject/CoreDataProject/CoreDataProject/SceneDelegate.swift
@@ -5,7 +5,6 @@
 //  Created by Néstor I. Martínez Ostoa on 07/01/20.
 //  Copyright © 2020 Néstor I. Martínez Ostoa. All rights reserved.
 //
-import CoreData
 import UIKit
 import SwiftUI
 
@@ -21,7 +20,6 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
         // Get the managed object context from the shared persistent container.
         let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
-        context.mergePolicy = NSMergeByPropertyObjectTrumpMergePolicy
 
         // Create the SwiftUI view and set the context as the value for the managedObjectContext environment keyPath.
         // Add `@Environment(\.managedObjectContext)` in the views that will need the context.
