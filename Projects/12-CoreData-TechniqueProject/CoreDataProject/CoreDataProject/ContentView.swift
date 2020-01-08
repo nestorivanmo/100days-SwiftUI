2eecf10d52f345ef2ed25258bfcb50f6d1a14f99
nestorivanmo <nestor.martinez.98@hotmail.com>
2020-01-07 18:34:00 -0600

    Creating NSManagedObject subclasses

--- Projects/12-CoreData-TechniqueProject/CoreDataProject/CoreDataProject/ContentView.swift
+++ Projects/12-CoreData-TechniqueProject/CoreDataProject/CoreDataProject/ContentView.swift
@@ -4,7 +4,7 @@
 //
 //  Created by Néstor I. Martínez Ostoa on 07/01/20.
 //  Copyright © 2020 Néstor I. Martínez Ostoa. All rights reserved.
-// 
+//
 
 import SwiftUI
 
---
+++ Projects/12-CoreData-TechniqueProject/CoreDataProject/CoreDataProject/Movie+CoreDataClass.swift
@@ -0,0 +1,16 @@
+//
+//  Movie+CoreDataClass.swift
+//  CoreDataProject
+//
+//  Created by Néstor I. Martínez Ostoa on 07/01/20.
+//  Copyright © 2020 Néstor I. Martínez Ostoa. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Movie)
+public class Movie: NSManagedObject {
+
+}
